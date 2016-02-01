7cfb08aa23f5218f3ad6590f60bfc400759c97ef
liuwencai <821272852@qq.com>
2016-02-01 19:48:54 +0800

    modify some user.go

--- controllers/user.go
+++ controllers/user.go
@@ -7,5 +7,8 @@ type UserController struct{
 	beego.Controller
 }
 func (c *UserController) Profile(){
+	c.Data["userid"]="wencai"
+	c.Data["tag"]="I am wencai"
+	c.Data["hobby"]=[] string{"run","code"}
 	c.TplName = "user/profile.html"
 }
